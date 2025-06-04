1cc01e8c383ea03bee25b1b500ce4fc637480716
hisham.nasrallah <hisham.nasralla@gmail.com>
2025-06-05 00:46:51 +0300

    first

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthService } from './services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-root',
